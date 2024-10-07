070d9ea1eb12bef7b99ecff910e3d8aecb22549b
name-88 <sasha33433@gmail.com>
2024-10-07 10:48:34 +0300

    тэг из выпадающего списка

--- cataas.py
+++ cataas.py
@@ -1,9 +1,13 @@
-from cProfile import label
+from tkinter import ttk
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+# Список доступных тегов
+ALLOWED_TAGS = [
+    'sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal'
+]
 
 def load_image(url):
     try:
@@ -36,33 +40,43 @@ def open_new_window():
         label.pack()
 
 
-def exit_app():
-    window.destroy()
+def open_new_window():
+    tag = tag_combobox.get()
+    url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_with_tag)
+    if img:
+        new_window = Toplevel()
+        new_window.title("Cat Image")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.image = img
+        label.pack()
 
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
-# Создаем меню
+
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
-# Добавляем пункты меню
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
 file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
-file_menu.add_command(label="Выход", command=exit_app)
+file_menu.add_command(label="Выход", command=window.destroy)
 
-# Поле ввода для тегов
-tag_entry = Entry()
-tag_entry.pack()
+# Метка "Выбери тег"
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=ALLOWED_TAGS)
+tag_combobox.pack()
 
 # Кнопка для загрузки изображения с тегом
 load_button = Button(text="Загрузить по тегу", command=open_new_window)
 load_button.pack()
 
 
-url = 'https://cataas.com/cat'
-
 window.mainloop()
+
 

47ce6f19ceda27fac2168b4483a4acd9438ddbc2
name-88 <sasha33433@gmail.com>
2024-10-07 09:43:59 +0300

    кнопка обновления фото

--- cataas.py
+++ cataas.py
@@ -4,10 +4,8 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from image.views import image
 
-
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -18,6 +16,12 @@ def load_image():
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
 
 
 window = Tk()
@@ -27,11 +31,16 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack
+
 url = "http://cataas.com/cat"
 img = load_image(url)
 
 if img:
     label.config(image=img)
     label.image = img
+
+set_image()
 
 window.mainloop()

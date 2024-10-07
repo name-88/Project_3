916a01c01fd74f7e4ac77ea0860045fa8e55ecea
name-88 <sasha33433@gmail.com>
2024-10-07 09:01:32 +0300

    рисуем главное окно

--- cataas.py
+++ cataas.py
@@ -1,1 +1,23 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+from pygame.examples.sprite_texture import load_img
+from будильник import window, label
+
+window = Tk()
+window.title("Cats")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "http://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
